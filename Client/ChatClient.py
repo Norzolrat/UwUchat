a477920dfacd4d7d0b89ccd4fbd33babcc3cb8dd
Enzo SA <sa.enzo@neuf.fr>
2023-03-15 00:33:53 +0100

    Update2

--- Client/ChatClient.py
+++ Client/ChatClient.py
@@ -1,49 +1,90 @@
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.primitives.asymmetric import padding
-from cryptography.hazmat.primitives import serialization, hashes
-from cryptography.hazmat.primitives import padding as text_padding
-from cryptography.hazmat.backends import default_backend
-import base64
-import os
 import socket
+import os
+from cryptography.hazmat.primitives.asymmetric import padding, rsa
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.backends import default_backend
 
-def send_to_server(encrypted_message, encrypted_aes_key):
-    HOST = '127.0.0.1'  # Adresse IP du serveur
-    PORT = 65432        # Port utilisé pour la communication
+SERVER_HOST = "127.0.0.1"
+SERVER_PORT = 1234
+
+# Fonction pour générer une clé AES aléatoire
+def generate_aes_key():
+    return os.urandom(32)
+
+# Fonction pour chiffrer un message avec une clé AES donnée
+def aes_encrypt(message, key):
+    iv = os.urandom(16)
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    ciphertext = encryptor.update(message) + encryptor.finalize()
+    return iv + ciphertext
+
+# Fonction pour déchiffrer un message avec une clé AES donnée
+def aes_decrypt(ciphertext, key):
+    iv = ciphertext[:16]
+    ciphertext = ciphertext[16:]
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    decryptor = cipher.decryptor()
+    plaintext = decryptor.update(ciphertext) + decryptor.finalize()
+    return plaintext
+
+# Fonction pour chiffrer une clé AES avec une clé publique RSA donnée
+def rsa_encrypt(key, public_key):
+    ciphertext = public_key.encrypt(
+        key,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return ciphertext
+
+# Fonction pour déchiffrer une clé AES avec une clé privée RSA donnée
+def rsa_decrypt(ciphertext, private_key):
+    plaintext = private_key.decrypt(
+        ciphertext,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return plaintext
+
+def main():
+    # Connexion au serveur
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        s.sendall(encrypted_message + b';' + encrypted_aes_key)
+        s.connect((SERVER_HOST, SERVER_PORT))
+        print("Connected to server")
 
-backend = default_backend()
+        # Réception de la clé publique du serveur
+        data = s.recv(1024)
+        server_public_key = serialization.load_pem_public_key(
+            data,
+            backend=default_backend()
+        )
 
-# Message à chiffrer
-message = b"Bonjour Bob, c'est Alice !"
+        # Génération d'une clé AES aléatoire pour le chiffrement des messages
+        aes_key = generate_aes_key()
 
-# Génération de la clé aléatoire AES
-key = Cipher(algorithms.AES(os.urandom(32)), modes.CBC(os.urandom(16)), backend=backend).encryptor()
+        # Envoi de la clé AES chiffrée avec la clé publique du serveur
+        aes_key_ciphertext = rsa_encrypt(aes_key, server_public_key)
+        s.sendall(aes_key_ciphertext)
 
-# Remplissage des données avec PKCS7
-padder = text_padding.PKCS7(algorithms.AES.block_size).padder()
-padded_data = padder.update(message) + padder.finalize()
+        # Envoi de messages chiffrés en AES au serveur
+        while True:
+            message = input("Enter message to send: ")
+            if message == "quit":
+                break
+            ciphertext = aes_encrypt(message.encode(), aes_key)
+            s.sendall(ciphertext)
+            data = s.recv(1024)
+            plaintext = aes_decrypt(data, aes_key)
+            print("Received data:", plaintext.decode())
 
-# Chiffrement du message en AES
-cipher_text = key.update(padded_data) + key.finalize()
+    print("Disconnected from server")
 
-# Ouverture de la clé publique du serveur
-with open("public_key.pem", "rb") as key_file:
-    public_key = serialization.load_pem_public_key(key_file.read(), backend=backend)
-
-# Génération de la clé aléatoire pour le chiffrement RSA de la clé AES
-rsa_key = Cipher(algorithms.AES(os.urandom(32)), modes.CBC(os.urandom(16)), backend=backend).encryptor()
-
-# Chiffrement de la clé AES en RSA avec la clé publique du serveur
-encrypted_aes_key = base64.b64encode(public_key.encrypt(rsa_key.finalize(),
-                        padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                                     algorithm=hashes.SHA256(),
-                                     label=None)))
-
-# Conversion en base64 du message chiffré en AES
-encrypted_message = base64.b64encode(cipher_text)
-
-# Envoi du message chiffré en AES et de la clé AES chiffrée en RSA au serveur
-send_to_server(encrypted_message, encrypted_aes_key)
+if __name__ == '__main__':
+    main()
--- Client/ChatServer.py
+++ Client/ChatServer.py
@@ -1,44 +1,48 @@
 import socket
 import os
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
-from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding, rsa
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import serialization, hashes
-from cryptography.hazmat.primitives import padding as text_padding
 from cryptography.hazmat.backends import default_backend
-import base64
-import os
-import socket
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+HOST = "127.0.0.1"
+PORT = 1234
 
-# Check if the private and public key files exist. If not, generate new keys.
-if not os.path.isfile("private_key.pem") and not os.path.isfile("public_key.pem"):
-    print("Generating new key pair...")
-    key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
-    private_key = key.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption()
-    )
-    public_key = key.public_key().public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-    )
-    with open("private_key.pem", "wb") as f:
-        f.write(private_key)
-    with open("public_key.pem", "wb") as f:
-        f.write(public_key)
+# Fonction pour générer une clé AES aléatoire
+def generate_aes_key():
+    return os.urandom(32)
 
+# Fonction pour chiffrer un message avec une clé AES donnée
+def aes_encrypt(message, key):
+    iv = os.urandom(16)
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    ciphertext = encryptor.update(message) + encryptor.finalize()
+    return iv + ciphertext
 
-def rsa_decrypt(ciphertext):
-    with open("private_key.pem", "rb") as f:
-        private_key = serialization.load_pem_private_key(
-            f.read(),
-            password=None
+# Fonction pour déchiffrer un message avec une clé AES donnée
+def aes_decrypt(ciphertext, key):
+    iv = ciphertext[:16]
+    ciphertext = ciphertext[16:]
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    decryptor = cipher.decryptor()
+    plaintext = decryptor.update(ciphertext) + decryptor.finalize()
+    return plaintext
+
+# Fonction pour chiffrer une clé AES avec une clé publique RSA donnée
+def rsa_encrypt(key, public_key):
+    ciphertext = public_key.encrypt(
+        key,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
         )
+    )
+    return ciphertext
+
+# Fonction pour déchiffrer une clé AES avec une clé privée RSA donnée
+def rsa_decrypt(ciphertext, private_key):
     plaintext = private_key.decrypt(
         ciphertext,
         padding.OAEP(
@@ -49,32 +53,40 @@ def rsa_decrypt(ciphertext):
     )
     return plaintext
 
+def main():
+    # Chargement de la clé privée RSA du serveur
+    with open("server_private_key.pem", "rb") as key_file:
+        server_private_key = serialization.load_pem_private_key(
+            key_file.read(),
+            password=None,
+            backend=default_backend()
+        )
 
-def aes_decrypt(ciphertext, key):
-    iv = ciphertext[:16]
-    ciphertext = ciphertext[16:]
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
-    decryptor = cipher.decryptor()
-    plaintext = decryptor.update(ciphertext) + decryptor.finalize()
-    unpadder = text_padding.PKCS7(algorithms.AES.block_size).unpadder()
-    plaintext = unpadder.update(plaintext) + unpadder.finalize()
-    return plaintext
+    # Création d'une socket TCP/IP
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        # Association de la socket à une adresse IP et un numéro de port
+        s.bind((HOST, PORT))
 
+        # Attente de connexion
+        s.listen()
+        print("Server listening on {}:{}".format(HOST, PORT))
 
+        # Attente de connexion d'un client
+        conn, addr = s.accept()
+        with conn:
+            print("Connected by", addr)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            print('Received data:', data)
-            rsa_plaintext = rsa_decrypt(data)
-            aes_key = rsa_plaintext[:32]
-            aes_ciphertext = rsa_plaintext[32:]
-            aes_plaintext = aes_decrypt(aes_ciphertext, aes_key)
-            print('Decrypted message:', aes_plaintext.decode())
+            # Envoi de la clé publique RSA du serveur
+            server_public_key = server_private_key.public_key().public_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PublicFormat.SubjectPublicKeyInfo
+            )
+            conn.sendall(server_public_key)
+
+            # Réception de la clé AES chiffrée avec la clé publique RSA du serveur
+            aes_key_ciphertext = conn.recv(1024)
+            aes_key = rsa_decrypt(aes_key_ciphertext, server_private_key)
+
+            # Réception de messages chiffrés en AES du client et envoi de réponses chiffrées
+            while True:
+                data = conn.recv
